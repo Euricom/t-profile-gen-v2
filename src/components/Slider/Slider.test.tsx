8be7ccfa50a7077e23dacaf3fa1fe5dc7141982b
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-09-14 14:16:10 +0200

    test: add unit tests for slider component

---
+++ src/components/Slider/Slider.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Slider from './Slider';
+
+describe('Slider component:', () => {
+  test('renders by default', () => {
+    const onChangeMock = jest.fn();
+
+    render(<Slider ariaLabel="test aria label" onChange={onChangeMock} />);
+
+    expect(screen.getByRole('slider', { name: /test aria label/i })).toBeVisible();
+  });
+
+  test('returns event on change', () => {
+    const onChangeMock = jest.fn();
+
+    render(<Slider ariaLabel="test aria label" onChange={onChangeMock} />);
+
+    const sliderElement = screen.getByRole('slider', { name: /test aria label/i });
+    fireEvent.change(sliderElement, { target: { value: 25 } });
+    expect(onChangeMock).toBeCalled();
+    expect(onChangeMock).toHaveBeenCalledTimes(1);
+  });
+});
