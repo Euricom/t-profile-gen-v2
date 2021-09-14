5258aba7c04e5ce840d4301ac3408a44cbab68c9
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-09-14 15:16:06 +0200

    refactor: add styles.ts for slider component

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,56 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import * as S from './styles'
 
-const sliderIcon =
-  'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjYiIGhlaWdodD0iMzgiIHZpZXdCb3g9IjAgMCAyNiAzOCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTAgMjhWMEgyNlYyOEwxMyAzOEwwIDI4WiIgZmlsbD0iIzZBQjE0QyIvPgo8L3N2Zz4K';
-
-const StyledSlider = styled.input.attrs(() => ({
-  type: 'range',
-}))`
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  appearance: none;
-  background: #000;
-  cursor: pointer;
-  height: 1px;
-  margin: 0;
-  opacity: 1;
-  width: 100%;
-
-  // Chrome, Opera, Safari, Edge
-  ::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    appearance: none;
-    background-image: url(${sliderIcon});
-    background-repeat: no-repeat;
-    background-size: contain;
-    border: none;
-    cursor: pointer;
-    height: 15px;
-    width: 10px;
-  }
-
-  // Firefox
-  ::-moz-range-thumb {
-    -moz-appearance: none;
-    appearance: none;
-    background-color: transparent;
-    background-image: url(${sliderIcon});
-    background-position: 50%;
-    border-radius: 0;
-    background-repeat: no-repeat;
-    background-size: contain;
-    border: none;
-    box-sizing: border-box;
-    cursor: pointer;
-    height: 15px;
-    width: 10px;
-  }
-
-  :hover {
-    opacity: 0.8;
-  }
-`;
 
 interface SliderProps {
   ariaLabel: string;
@@ -59,7 +9,7 @@ interface SliderProps {
 }
 
 const Slider = ({ ariaLabel, onChange, value }: SliderProps): JSX.Element => (
-  <StyledSlider aria-label={ariaLabel} onChange={(event) => onChange(event)} value={value} />
+  <S.Slider aria-label={ariaLabel} onChange={(event) => onChange(event)} value={value} />
 );
 
 export default Slider;
