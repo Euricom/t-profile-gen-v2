ed4165346c7c6e2c0e89d5e13a28d348bf2886fa
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-09-17 14:02:50 +0200

    feat: add GlobalStyle

---
+++ src/theme/globalStyles.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+//  global styles go here
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
+
+export default GlobalStyle;
