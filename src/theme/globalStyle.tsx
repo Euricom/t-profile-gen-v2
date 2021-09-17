732944b42b6d9fde7d8a5bc33e9d5377b8fea49d
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-09-17 14:18:02 +0200

    feat: add roboto font with fontsource

--- src/theme/globalStyle.tsx
+++ src/theme/globalStyle.tsx
@@ -1,9 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
 
+// fontsource import
+import '@fontsource/roboto/400.css'; // Roboto Regular
+import '@fontsource/roboto/700.css'; // Roboto Bold
+
 //  global styles go here
 const GlobalStyle = createGlobalStyle`
-  ${reset}
+  ${reset};  
+  body {
+    font-family : Roboto, Helvetica Neue, Arial, sans-serif
+  }
 `;
 
 export default GlobalStyle;
