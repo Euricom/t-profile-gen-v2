a5b331829f81f851e85f6915141d00c88da389df
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-09-17 14:03:57 +0200

    fix: GlobalStyle typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import GlobalStyle from './theme/globalStyles';
+import GlobalStyle from './theme/globalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
