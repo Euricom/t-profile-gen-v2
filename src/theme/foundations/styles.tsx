b0629f2a57e327d3f18813a88bec3d15362c6845
Tim Francois <tim.francois96@gmail.com>
2021-09-20 15:56:13 +0200

    all elements border-box + no outlines

--- src/theme/foundations/styles.tsx
+++ src/theme/foundations/styles.tsx
@@ -8,6 +8,12 @@ import '@fontsource/roboto/700.css'; // Roboto Bold
 //  global styles go here
 const GlobalStyles = createGlobalStyle`
   ${reset};  
+
+  * {
+    box-sizing: border-box;
+    outline: none;
+  }
+
   body {
     font-family : Roboto, Helvetica Neue, Arial, sans-serif
   }
