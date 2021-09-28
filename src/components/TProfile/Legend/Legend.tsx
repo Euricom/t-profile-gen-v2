46fb23ce8fde77c37ccedf646ce6b2e9f5646bdd
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-09-28 10:35:03 +0200

    wip: add show legend item only below 25% proficiency

--- src/components/TProfile/Legend/Legend.tsx
+++ src/components/TProfile/Legend/Legend.tsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import * as S from './styles';
 
+const proficiencyValue = 25;
+
 const Legend = (): JSX.Element => {
   const SkillSetPlaceholder = {
-    g1: { proficiency: 0, skill: 'React' },
-    g2: { proficiency: 0, skill: 'Angular' },
-    g3: { proficiency: 0, skill: 'Vue' },
-    s1: { proficiency: 0, skill: 'Back-End' },
-    s2: { proficiency: 0, skill: 'Devops' },
+    g1: { proficiency: 25, skill: 'React' },
+    g2: { proficiency: 24, skill: 'Angular' },
+    g3: { proficiency: 100, skill: 'Vue' },
+    s1: { proficiency: 40, skill: 'Back-End' },
+    s2: { proficiency: 10, skill: 'Devops' },
     s3: { proficiency: 0, skill: 'Data Science' },
   };
 
   const legendItems = Object.entries(SkillSetPlaceholder).map((legendItem) => {
     const abbr = legendItem[0];
-    const { skill } = legendItem[1];
+    const { proficiency, skill } = legendItem[1];
 
     return {
       abbr,
+      proficiency,
       skill,
     };
   });
 
   return (
     <S.Legend>
-      {legendItems.map((legendItem) => (
-        <S.LegendItem>
-          {legendItem.abbr.toUpperCase()}: <span>{legendItem.skill}</span>
-        </S.LegendItem>
-      ))}
+      {legendItems.map((legendItem) => {
+        if (legendItem.skill && legendItem.proficiency >= proficiencyValue) {
+          return (
+            <S.LegendItem key={legendItem.abbr}>
+              {legendItem.abbr.toUpperCase()}: <span>{legendItem.skill}</span>
+            </S.LegendItem>
+          );
+        }
+        return null;
+      })}
     </S.Legend>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { GlobalStyle, theme } from './theme';
+import SkillProvider from './contexts/skills';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <SkillProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </SkillProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
